6a31407cb0f6ced9b58df0dc2274556d601d1be8
Jeremy <hurleyjer@yahoo.com>
2017-08-18 14:05:34 -0400

    init commit

---
+++ src/app/recipe.ts
@@ -0,0 +1,9 @@
+import { Ingredient } from "app/ingredient";
+
+export class Recipe {
+        constructor(
+            public title: string,
+            public ingredients: Ingredient[],
+            public servingSize: number
+        ){}
+}
